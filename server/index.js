227d9246a734a801d540247eafa31c509cbc8e17
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2023-09-28 22:26:30 +0700

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database connected successfully!!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
